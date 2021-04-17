4e58a51daab1b2b66a52737635959a29c9c85233
Mai Ba Long <69111065+MaiBaLong154501@users.noreply.github.com>
2021-04-17 14:31:03 +0700

    Add files via upload

---
+++ Halstead/Halstead/GUI/frm_Main.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Halstead.GUI
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Path = new System.Windows.Forms.TextBox();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.txt_Files = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txt_Result = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dtg_Operator = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.dtg_Operand = new System.Windows.Forms.DataGridView();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.cbox_Folder = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_Operator)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_Operand)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_Path
+            // 
+            this.txt_Path.Location = new System.Drawing.Point(314, 28);
+            this.txt_Path.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Path.Name = "txt_Path";
+            this.txt_Path.Size = new System.Drawing.Size(346, 22);
+            this.txt_Path.TabIndex = 0;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btn_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_Add.Location = new System.Drawing.Point(764, 25);
+            this.btn_Add.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(124, 28);
+            this.btn_Add.TabIndex = 1;
+            this.btn_Add.Text = "Choose Folder";
+            this.btn_Add.UseVisualStyleBackColor = false;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Refresh.Location = new System.Drawing.Point(764, 83);
+            this.btn_Refresh.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(124, 28);
+            this.btn_Refresh.TabIndex = 2;
+            this.btn_Refresh.Text = "Refresh";
+            this.btn_Refresh.UseVisualStyleBackColor = true;
+            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(16, 58);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(740, 481);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txt_Files);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(732, 452);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Files";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // txt_Files
+            // 
+            this.txt_Files.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Files.Location = new System.Drawing.Point(0, 0);
+            this.txt_Files.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Files.Multiline = true;
+            this.txt_Files.Name = "txt_Files";
+            this.txt_Files.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_Files.Size = new System.Drawing.Size(736, 456);
+            this.txt_Files.TabIndex = 0;
+            this.txt_Files.WordWrap = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txt_Result);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(732, 452);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Halstead";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txt_Result
+            // 
+            this.txt_Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Result.Location = new System.Drawing.Point(0, 0);
+            this.txt_Result.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_Result.Multiline = true;
+            this.txt_Result.Name = "txt_Result";
+            this.txt_Result.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_Result.Size = new System.Drawing.Size(736, 456);
+            this.txt_Result.TabIndex = 0;
+            this.txt_Result.WordWrap = false;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dtg_Operator);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage3.Size = new System.Drawing.Size(732, 452);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Operators";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dtg_Operator
+            // 
+            this.dtg_Operator.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_Operator.Location = new System.Drawing.Point(0, 0);
+            this.dtg_Operator.Margin = new System.Windows.Forms.Padding(4);
+            this.dtg_Operator.Name = "dtg_Operator";
+            this.dtg_Operator.RowHeadersWidth = 51;
+            this.dtg_Operator.Size = new System.Drawing.Size(732, 452);
+            this.dtg_Operator.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.dtg_Operand);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage4.Size = new System.Drawing.Size(732, 452);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Operands";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // dtg_Operand
+            // 
+            this.dtg_Operand.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_Operand.Location = new System.Drawing.Point(0, 0);
+            this.dtg_Operand.Margin = new System.Windows.Forms.Padding(4);
+            this.dtg_Operand.Name = "dtg_Operand";
+            this.dtg_Operand.RowHeadersWidth = 51;
+            this.dtg_Operand.Size = new System.Drawing.Size(732, 452);
+            this.dtg_Operand.TabIndex = 0;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Save.Location = new System.Drawing.Point(764, 146);
+            this.btn_Save.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(124, 28);
+            this.btn_Save.TabIndex = 4;
+            this.btn_Save.Text = "Export";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // cbox_Folder
+            // 
+            this.cbox_Folder.AutoSize = true;
+            this.cbox_Folder.Location = new System.Drawing.Point(667, 30);
+            this.cbox_Folder.Name = "cbox_Folder";
+            this.cbox_Folder.Size = new System.Drawing.Size(70, 21);
+            this.cbox_Folder.TabIndex = 5;
+            this.cbox_Folder.Text = "Folder";
+            this.cbox_Folder.UseVisualStyleBackColor = true;
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 582);
+            this.Controls.Add(this.cbox_Folder);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btn_Refresh);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.txt_Path);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frm_Main";
+            this.Text = "Halstead Complexity Metric";
+            this.Load += new System.EventHandler(this.frm_Main_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_Operator)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_Operand)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Path;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Refresh;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox txt_Files;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TextBox txt_Result;
+        private System.Windows.Forms.DataGridView dtg_Operator;
+        private System.Windows.Forms.DataGridView dtg_Operand;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.CheckBox cbox_Folder;
+    }
+}
---
+++ Halstead/Halstead/GUI/frm_Main.cs
@@ -0,0 +1,284 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Halstead.GUI
+{
+    public partial class frm_Main : Form
+    {
+        private string[] fe = { "*.cs", "*.java", "*.c", "*.cpp", "*.js" };
+        private String[] files;
+        private Dictionary<string, int> listOperator;
+        private Dictionary<string, int> listOperand;
+        private source.HalsteadMethod halstead;
+
+        public frm_Main()
+        {
+            this.listOperator = new Dictionary<string, int>();
+            this.listOperand = new Dictionary<string, int>();
+            halstead = new source.HalsteadMethod(listOperator, listOperand);
+            files = null;
+            InitializeComponent();
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowerDialog = new FolderBrowserDialog();
+            folderBrowerDialog.Description = "Custom Description";
+
+            if (folderBrowerDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.txt_Path.Text = folderBrowerDialog.SelectedPath;
+            }
+            String folder = this.txt_Path.Text;
+            try
+            {
+                List<string> list = new List<string>();
+                foreach (string s in fe)
+                {
+                    string[] f = Directory.GetFiles(folder, s, SearchOption.AllDirectories);
+                    foreach (string s1 in f)
+                        list.Add(s1);
+                }
+                this.files = list.ToArray();
+                
+            }
+            catch (Exception err)
+            {
+                Debug.Print(err.ToString());
+            }
+            this.Analyse();
+        }
+
+        private void Analyse()
+        {
+            this.listOperator = new Dictionary<string, int>();
+            this.listOperand = new Dictionary<string, int>();
+            try
+            {
+                if (files != null)
+                {
+                    foreach (String f in files)
+                    {
+                        try
+                        {
+                                source.Counting calc = new source.Counting(f, listOperator, listOperand);
+
+                                this.listOperator = calc.listOperator;
+                                this.listOperand = calc.listOperand;
+
+                                halstead = new source.HalsteadMethod(this.listOperator, this.listOperand);
+
+                                txt_Files.AppendText(f + Environment.NewLine);
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Lỗi. Làm phiền bạn thử lại sau!!!");
+                        }
+                    }
+                    ViewResult();
+                    ViewOperand();
+                    ViewOperator();
+                }
+            }
+            catch (Exception err)
+            {
+                Debug.Print(err.ToString());
+            }
+        }
+
+        private void ViewResult()
+        {
+            String results = "";
+            results += "+ Number of distinct operators in the program(μ1): " + halstead.NumberOfOperators;
+            results += "\r\n+ Number of distinct operands in the program(μ2): " + halstead.NumberOfOperands;
+            results += "\r\n+ Total number of occurrences of operators in the program(N1): " + halstead.TotalOperators;
+            results += "\r\n+ Total number of occurrences of operands in the program(N2): " + halstead.TotalOperands;
+            results += "\r\n+ Program vocabulary(μ): " + halstead.ProgramVocaburary();
+            results += "\r\n+ Program length(N): " + halstead.ProgramLength();
+            results += "\r\n+ Program volume(V): " + halstead.ProgramVolume().ToString("0.#####");
+            results += "\r\n+ Program difficulty(D): " + halstead.ProgramDifficulty().ToString("0.#####");
+            results += "\r\n+ Program level(L): " + halstead.ProgramLevel().ToString("0.#####");
+            results += "\r\n+ Program estimate length: " + halstead.ProgramEstimatedLength().ToString("0.#####");
+            results += "\r\n+ Effort: " + halstead.CalculateEffort().ToString("0.#####");
+            results += "\r\n+ Time: " + halstead.CalculateTime().ToString("0.#####");
+            results += "\r\n+ Remaining Bugs: " + halstead.CalculateRemainingBugs().ToString("0.#####");
+
+            this.txt_Result.Text = results;
+        }
+
+        private void ViewOperator()
+        {
+            dtg_Operator.DataSource = (from d in listOperator orderby d.Value select new { d.Key, d.Value }).ToList();
+            this.dtg_Operator.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtg_Operator.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+
+            this.dtg_Operator.BorderStyle = BorderStyle.None;
+            this.dtg_Operator.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(238, 239, 249);
+            this.dtg_Operator.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dtg_Operator.DefaultCellStyle.SelectionBackColor = Color.DarkTurquoise;
+            this.dtg_Operator.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;
+            this.dtg_Operator.BackgroundColor = Color.White;
+            this.dtg_Operator.EnableHeadersVisualStyles = false;
+            this.dtg_Operator.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            this.dtg_Operator.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(20, 25, 72);
+            this.dtg_Operator.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+
+            this.dtg_Operator.AllowUserToAddRows = false;
+        }
+
+        private void ViewOperand()
+        {
+            dtg_Operand.DataSource = (from d in listOperand orderby d.Value select new { d.Key, d.Value }).ToList();
+            this.dtg_Operand.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtg_Operand.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+
+            this.dtg_Operand.BorderStyle = BorderStyle.None;
+            this.dtg_Operand.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(238, 239, 249);
+            this.dtg_Operand.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dtg_Operand.DefaultCellStyle.SelectionBackColor = Color.DarkTurquoise;
+            this.dtg_Operand.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;
+            this.dtg_Operand.BackgroundColor = Color.White;
+            this.dtg_Operand.EnableHeadersVisualStyles = false;
+            this.dtg_Operand.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            this.dtg_Operand.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(20, 25, 72);
+            this.dtg_Operand.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+
+            this.dtg_Operand.AllowUserToAddRows = false;
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            this.listOperator = new Dictionary<string, int>();
+            this.listOperand = new Dictionary<string, int>();
+            halstead = new source.HalsteadMethod(listOperator, listOperand);
+            files = null;
+            txt_Files.Text = "";
+            txt_Path.Text = "";
+            txt_Result.Text = "";
+            this.dtg_Operand.DataSource = null;
+            this.dtg_Operand.Rows.Clear();
+
+            this.dtg_Operator.DataSource = null;
+            this.dtg_Operator.Rows.Clear();
+
+        }
+
+        private void frm_Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "PDF file|*.pdf", ValidateNames = true })
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    iTextSharp.text.Document doc = new iTextSharp.text.Document(PageSize.A4.Rotate());
+                    try
+                    {
+                        Chunk chunk = new Chunk("Count Operand", FontFactory.GetFont("dax-black"));
+                        chunk.SetUnderline(0.5f, -1.5f);
+                        PdfWriter.GetInstance(doc, new FileStream(sfd.FileName, FileMode.Create));
+                        doc.Open();
+
+                        doc.Add(new Paragraph("Operator"));
+                        doc.Add(new Paragraph(" "));
+                        // Export to PDF in Windows Forms DataGrid Operator
+                        PdfPTable pdfTable = new PdfPTable(dtg_Operator.ColumnCount);
+                        pdfTable.DefaultCell.Padding = 3;
+                        pdfTable.WidthPercentage = 70;
+                        pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+                        pdfTable.DefaultCell.BorderWidth = 1;
+                        //Adding Header row
+                        foreach (DataGridViewColumn column in dtg_Operator.Columns)
+                        {
+                            PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                            cell.BackgroundColor = new iTextSharp.text.BaseColor(240, 240, 240);
+                            pdfTable.AddCell(cell);
+                        }
+                        //Adding DataRow
+                        foreach (DataGridViewRow row in dtg_Operator.Rows)
+                        {
+                            foreach (DataGridViewCell cell in row.Cells)
+                            {
+                                if (cell.Value == null)
+                                {
+
+                                }
+                                else
+                                {
+                                    pdfTable.AddCell(cell.Value.ToString());
+
+                                }
+                            }
+                        }
+                        doc.Add(pdfTable);
+
+                        doc.Add(new Paragraph(" "));
+                        doc.Add(new Paragraph("Operand"));
+                        doc.Add(new Paragraph(" "));
+                        // Export to PDF in Windows Forms DataGrid Operand  
+                        PdfPTable pdf_Operand = new PdfPTable(dtg_Operand.ColumnCount);
+                        pdf_Operand.DefaultCell.Padding = 3;
+                        pdf_Operand.WidthPercentage = 70;
+                        pdf_Operand.HorizontalAlignment = Element.ALIGN_LEFT;
+                        pdf_Operand.DefaultCell.BorderWidth = 1;
+                        //Adding Header row
+                        foreach (DataGridViewColumn column in dtg_Operand.Columns)
+                        {
+                            PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                            cell.BackgroundColor = new iTextSharp.text.BaseColor(240, 240, 240);
+                            pdf_Operand.AddCell(cell);
+                        }
+                        //Adding DataRow
+                        foreach (DataGridViewRow row in dtg_Operand.Rows)
+                        {
+                            foreach (DataGridViewCell cell in row.Cells)
+                            {
+                                if (cell.Value == null)
+                                {
+
+                                }
+                                else
+                                {
+                                    pdf_Operand.AddCell(cell.Value.ToString());
+
+                                }
+                            }
+                        }
+                        doc.Add(pdf_Operand);
+
+                        //doc.Add(new iTextSharp.text.Paragraph(txt_Files.Text));
+                        doc.Add(new Paragraph("Calculate"));
+                        doc.Add(new Paragraph(" "));
+                        doc.Add(new iTextSharp.text.Paragraph(txt_Result.Text));
+                    }
+
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        doc.Close();
+                    }
+                }
+            }
+        }
+    }
+}
